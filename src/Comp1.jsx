9314a68f193a9292aa3100e21540647c48daa37a
CruzParraAndres <161503044+CruzParraAndres@users.noreply.github.com>
2024-06-07 19:08:36 +0000

    Pending changes exported from your codespace

---
+++ src/Comp1.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function Comp1(){
+    return(
+        <div className='main'>
+            <h1 className="Gret">Cruz Parra Boris Andres Paralelo A</h1>
+            <p>Today´s a great day to life!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias officia pariatur cum id amet corrupti iure explicabo autem nostrum ex libero voluptatibus atque, neque porro repellendus illum rem magni? Ipsa?</p>
+            <div>
+            <img src="Octocat.png" className="App-logo" alt="logo" />
+            </div>
+        </div>
+    );
+}
+export default Comp1;
